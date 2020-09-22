8fbf19bc5b387007807e4c2e36af02798cda9a30
Nicola Zanon <nicola.zanon86@gmail.com>
2020-09-22 21:31:42 +0200

    Add readme and robots

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="noindex" />
       </Head>
 
       <main className={styles.main}>
